3749faee145e9bae4151825a27ee685876a247ab
s.h kim <76585539+ajou9770@users.noreply.github.com>
2022-03-08 16:50:42 +0900

    nomad

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-const title = document.querySelector(".title h1");
-const title2 = document.querySelector(".title h2");
-const title3 = document.querySelector(".title h3");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = loginForm.querySelector("button");
 
-console.log(title);
+const link = document.querySelector("a");
 
-function handleMouseEnter() {
-    console.log("mouse is enter");
-    title.style.color="green";
+function handleLinkClick(event) {
+    event.preventDefault();
+     
+}
+function onLoginSumbit(event) {
+    event.preventDefault();
+const userName = loginInput.value;
+console.log(event);
 }
 
-
-function handleMouseLeave() {
-    console.log("mouse is leaver");
-    title.style.color="black";
-}
-function handleTitle2Click() {
-    title2.style.color="yellow";
-}
-function handleTitle3Click() {
-    title3.style.color="blue";
-}
-
-title2.addEventListener("click", handleTitle2Click);
-title3.addEventListener("click", handleTitle3Click);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+loginForm.addEventListener("submit", onLoginSumbit);
+link.addEventListener("click", handleLinkClick);
