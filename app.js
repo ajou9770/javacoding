b5b0c71febd5f04712f7870b771f3ad734558d14
s.h kim <76585539+ajou9770@users.noreply.github.com>
2022-03-08 10:06:18 +0900

    app change

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-const title = document.getElementById("title");
-title.innerText = "do it";
-console.log(title.id);
-console.log(title.className);
+const title = document.querySelector(".title h1");
+const title2 = document.querySelector(".title h2");
+const title3 = document.querySelector(".title h3");
+
+console.log(title);
+
+function handleMouseEnter() {
+    console.log("mouse is enter");
+    title.style.color="green";
+}
+
+
+function handleMouseLeave() {
+    console.log("mouse is leaver");
+    title.style.color="black";
+}
+function handleTitle2Click() {
+    title2.style.color="yellow";
+}
+function handleTitle3Click() {
+    title3.style.color="blue";
+}
+
+title2.addEventListener("click", handleTitle2Click);
+title3.addEventListener("click", handleTitle3Click);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
